d91f1c37eda919693c323362b1624f0e9f9c8e49
tmbenhura <tmbenhura@gmail.com>
2023-03-29 00:54:03 +0200

    Package has a webhook configuration

--- config/order-status-notifications.php
+++ config/order-status-notifications.php
@@ -11,8 +11,18 @@ return [
     | Order Model
     |--------------------------------------------------------------------------
     |
-    | The order model to monitor for changes
+    | The order model to monitor for changes.
     |
     */
     'model' => env('OSN_MODEL', 'App\\Models\\Order'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Webhook URL
+    |--------------------------------------------------------------------------
+    |
+    | The user for MS Teams webhook.
+    |
+    */
+    'webhook_url' => env('OSN_WEBHOOK_URL', ''),
 ];
--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -5,3 +5,7 @@ declare(strict_types=1);
 test('package has a default model configured', function () {
     expect(config('order-status-notifications.model'))->toBe('App\\Models\\Order');
 });
+
+test('package has a webhook configuration', function () {
+    expect(config('order-status-notifications.webhook_url'))->toBe('');
+});
